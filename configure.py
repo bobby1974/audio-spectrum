5cfc5c2283297e1b273a50a0a95c2cccb83e58ed
bobbypiggy <tanguanzhong@foxmail.com>
2016-12-30 09:31:54 -0600

    Add files via upload

---
+++ configure.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# |----------------------------------------------------------------------| #
+# | Copyright (C) Bobby, BH7PRM, 2016                                    | #
+# |                                                                      | #
+# | This program is free software: you can redistribute it and/or modify | #
+# | it under the terms of the GNU General Public License as published by | #
+# | the Free Software Foundation, either version 3 of the License, or    | #
+# | any later version.                                                   | #
+# |                                                                      | #
+# | This program is distributed in the hope that it will be useful,      | #
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of       | #
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        | #
+# | GNU General Public License for more details.                         | #
+# |                                                                      | #
+# | You should have received a copy of the GNU General Public License    | #
+# | along with this program.  If not, see <http://www.gnu.org/licenses/>.| #
+# |----------------------------------------------------------------------| #
+
+import pyaudio
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+
+SOUND_CARD_CHUNK = 1024
+PYAUDIO_FORMAT_INT16 = pyaudio.paInt16
+SOUND_CARD_CHANNELS = 1
+SOUND_CARD_RATE = 8000
+RECORD_SECONDS = 5
